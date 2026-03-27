b9ef1627e3f4d46379ec1da05a3ed437225a2365
SrimansiRamesh <srimansi1307@gmail.com>
2026-03-27 18:27:19 -0400

    changes to resolve CORS

--- backend/main.py
+++ backend/main.py
@@ -71,7 +71,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=[
+        "https://immigration-rag.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
